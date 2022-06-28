5dc8a5c53bb3e24b75ec388c50106e2d0f1123a7
jagdeep tanwar <jtanwar89@touchlogic.com>
2022-06-28 01:14:24 -0400

    ConfigurationClassUpdate

---
+++ WookieBooks/Data/EntityConfigurations/BookConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WookieBooks.Api.Entities;
+
+namespace WookieBooks.Api.Data.EntityConfigurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Title)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            builder.Property(c => c.Description)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+            builder.Property(c => c.Author)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            builder.Property(c => c.CoverImage)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            //builder.HasData(new Book
+            //{
+            //    Id = 1,
+            //    Title = "Data Structure",
+            //    Description = "Data Structure by Homer",
+            //    Author = "Homer Simpson",
+            //    CoverImage = "images/ds.png",
+            //    Price = 89.99
+            //});
+            //builder.HasData(new Book
+            //{
+            //    Id = 2,
+            //    Title = "Design Patterns",
+            //    Description = "Design Patterns by Peter",
+            //    Author = "Peter Griffin",
+            //    CoverImage = "images/dp.png",
+            //    Price = 99
+            //});
+
+        }
+    }
+}
--- WookieBooks/Data/WookieBooksDbContext.cs
+++ WookieBooks/Data/WookieBooksDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WookieBooks.Api.Data.EntityConfigurations;
 using WookieBooks.Api.Entities;
 
 namespace WookieBooks.Api.Data
@@ -9,8 +10,38 @@ namespace WookieBooks.Api.Data
         public WookieBooksDbContext(DbContextOptions<WookieBooksDbContext> options)
             : base(options)
         {
+
         }
 
         public  DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+        }
+
+        //public void LoadBooks()
+        //{
+        //    Book book = new Book
+        //    {
+        //        Id = 1,
+        //        Title = "Data Structure",
+        //        Description = "Data Structure by Homer",
+        //        Author = "Homer Simpson",
+        //        CoverImage = "images/ds.png",
+        //        Price = 89.99
+        //    };
+        //    Books.Add(book);
+        //    book = new Book
+        //    {
+        //        Id = 2,
+        //        Title = "Design Patterns",
+        //        Description = "Design Patterns by Peter",
+        //        Author = "Peter Griffin",
+        //        CoverImage = "images/dp.png",
+        //        Price = 99
+        //    };
+        //    Books.Add(book);
+        //}
     }
 }
